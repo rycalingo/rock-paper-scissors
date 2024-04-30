9f2149001c7b918495294ab282cd957ee68495e6
Ryan Calingo <rycalingo@gmail.com>
2024-04-30 19:08:31 -0400

    Update vite config setting

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/rock-paper-scissors",
   test: {
     globals: true,
     environment: 'jsdom',
